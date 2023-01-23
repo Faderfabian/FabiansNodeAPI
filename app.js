2fb9b904ac544547be357c307b14ed0dddd68733
Faderfabian <karl.fabian.alonso.jonsson@iths.se>
2023-01-23 11:57:09 +0100

    uppdaterat

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.static('public'))
 const PORT = process.env.PORT || 3000
 const https = require("https")
 
@@ -27,7 +28,7 @@ app.get("/instruments", (req, res) => {
 
 
 app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/index.html")
+    
 })
 
 
